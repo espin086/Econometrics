34ba6c95efdc521d3700d9fb99ca329ec4067674
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2024-04-11 12:07:07 -0700

    updated diff_in_diff.py

--- diff_in_diff.py
+++ diff_in_diff.py
@@ -1,18 +1,14 @@
 import argparse
 import pandas as pd
-import argparse
-import pandas as pd
-
-import statsmodels.api as sm
 
 import statsmodels.api as sm
 
 
-def run_diff_in_diff(data, treatment, post, outcome_variable):
+def run_diff_in_diff(data, treatment, post, outcome_variable, confounding_variables):
     """compute difference-in-differences estimate"""
     data["treatment_post"] = data[treatment] * data[post]
 
-    X = data[[treatment, post, "treatment_post"]]
+    X = data[[treatment, post, "treatment_post"] + confounding_variables]
     X = sm.add_constant(X)
 
     model = sm.OLS(data[outcome_variable], X).fit()
@@ -27,10 +23,19 @@ if __name__ == "__main__":
     parser.add_argument("treatment", type=str, help="Treatment variable")
     parser.add_argument("post", type=str, help="Post variable")
     parser.add_argument("outcome_variable", type=str, help="Outcome variable")
+    parser.add_argument(
+        "confounding_variables", nargs="*", help="List of confounding variables"
+    )
 
     args = parser.parse_args()
 
     data = pd.read_excel(args.data_file)  # Assuming data is in Excel format
 
-    result = run_diff_in_diff(data, args.treatment, args.post, args.outcome_variable)
+    result = run_diff_in_diff(
+        data,
+        args.treatment,
+        args.post,
+        args.outcome_variable,
+        args.confounding_variables,
+    )
     print(result)
